c90e3e23364a344a54dc6a32919bd479c9b05fa7
liorlevy888 <liorlevy888@gmail.com>
2020-05-05 09:13:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 TOKEN = 'a9852be06be949f48dd3e16d271f9bc5'
 AWS = 'Rh30BNyj+qBI4htYCtatLbNC3X1Nn42QtZkKMzJ9'
+GOOGLE_API = 'AIzaSyDMTUkJAmi1ahsx9uCGSgmcSmqDTBF9ygg'
 if name == "__main__":
   print "This is a test"
